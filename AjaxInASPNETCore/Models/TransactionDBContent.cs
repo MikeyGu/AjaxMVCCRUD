0e68d5135e086a7aeb500bbe2ee0582544c0bf6e
MikeyGu <68223569+MikeyGu@users.noreply.github.com>
2020-10-25 23:52:27 +0800

    10/25
    Add

---
+++ AjaxInASPNETCore/Models/TransactionDBContent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxInASPNETCore.Models
+{
+    public class TransactionDbContext: DbContext //繼承DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
